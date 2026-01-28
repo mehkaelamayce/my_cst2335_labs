043ca5e6f7bc595dad8f143ee81f8a80303dd6cf
Mehkaela Dimaano <dima0062@algonquinlive.com>
2026-01-27 23:09:10 -0500

    added a textfield

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var myFontSize = 30.0;
-  var _counter = 0.0;
+  var _counter = 30.0;
   var isChecked = false;
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    controller.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -118,7 +135,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child:
               Semantics(
                 label: "a counter of the number of times that the button was pressed",
-            child: Text('You have pushed the button this many times:',
+            child: Text(controller.value.text,
                 style: TextStyle(fontSize: myFontSize),
             )
             )
@@ -128,7 +145,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Image.asset("images/algonquin.jpg", width: 300.0, height: 300.0),
               label:"An image of the library at Algonquin College"),
 
-            ElevatedButton(onPressed: ( ) { setState(() {
+            ElevatedButton(onPressed: ( ) {
+              var typed = controller.value.text;
+              setState(() {
 
             }); myFontSize=20.0; },
                 child:Image.asset("images/algonquin.jpg", width: 60.0, height: 60.0)),
@@ -141,6 +160,8 @@ class _MyHomePageState extends State<MyHomePage> {
             Checkbox(value: isChecked,
                 onChanged: (newChecked) {
                   if(newChecked != null) {
+                    controller.text = "You checked the checkbox";
+
                     setState(() {
                       isChecked = newChecked;
                     });
@@ -149,6 +170,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 } ),
 
             Switch(value:isChecked,
+              activeThumbColor: Colors.yellow,
+
               onChanged: ( newChecked){
                   setState(() {
                     isChecked = newChecked;
@@ -156,7 +179,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
               },),
 
-            Slider(value: myFontSize, onChanged: setNewValue, min: 0.0, max: 100.0,)
+          TextField(controller: controller,
+          decoration:InputDecoration(
+            hintText: "Type in here",
+            labelText: "your text",
+            border: OutlineInputBorder(),
+          )
+
+          )
           ],
         ),
       ),
