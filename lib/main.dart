9ebd2fe61065978986f8a0bc2374f1948e380035
Mehkaela Dimaano <dima0062@algonquinlive.com>
2026-02-16 19:21:57 -0500

    accidentally made some changes and fixed it

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 
 
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -95,12 +97,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final typedPassword = passwordController.text;
 
     final bool? shouldSave = await showDialog<bool>(
-      context: context,
-      barrierDismissible: false,
+      context: context, barrierDismissible: false,
       builder: (context) => AlertDialog(
         title: const Text("Save login?"),
-        content: const Text(
-          "Do you want to save your username and password for next time?",
+        content: const Text( "Do you want to save your username and password for next time?",
         ),
         actions: [
           TextButton(
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
     if (shouldSave == true) {
       await saveCredentials();
     } else {
-      await clearCredentials(); // next app start will be empty
+      await clearCredentials();
     }
 
     setState(() {
@@ -129,58 +129,59 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Login Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                controller: loginController,
-                decoration: const InputDecoration(
-                    labelText: "Login",
-                    border: OutlineInputBorder()
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: const Text("Login Page"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: loginController,
+                    decoration: const InputDecoration(
+                        labelText: "Login",
+                        border: OutlineInputBorder()
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder()
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder()
+                    ),
+                  ),
                 ),
-              ),
+
+                ElevatedButton(
+                  onPressed: onLoginPressed,
+                  child: const Text("Login"),
+                ),
+
+                const SizedBox(height: 20),
+
+                Semantics(
+                  label: "Login result image",
+                  child: Image.asset(
+                    imageSource,
+                    height: 300,
+                    width: 300,
+                    fit: BoxFit.contain,
+                  ),
+                )
+              ],
             ),
+          ),
+        );
+      }
+    }
 
-            ElevatedButton(
-              onPressed: onLoginPressed,
-              child: const Text("Login"),
-            ),
-
-            const SizedBox(height: 20),
-
-            Semantics(
-              label: "Login result image",
-              child: Image.asset(
-                imageSource,
-                height: 300,
-                width: 300,
-                fit: BoxFit.contain,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
