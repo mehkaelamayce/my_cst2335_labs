0c7e9f5d151532e42ab4cac4144fc02583e64e70
Mehkaela Dimaano <dima0062@algonquinlive.com>
2026-02-24 07:47:42 -0500

    add controllers and list state for Shopping List

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,6 +1,31 @@
+import 'package:flutter/material.dart';
+
 class ShoppingItem {
   final String name;
   final int qty;
 
   ShoppingItem({required this.name, required this.qty});
+}
+
+  class ShoppingListPage extends StatefulWidget {
+    const ShoppingListPage({super.key});
+
+    @override
+    State<ShoppingListPage> createState() => _ShoppingListPageState();
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  final TextEditingController itemController = TextEditingController();
+  final TextEditingController qtyController = TextEditingController();
+
+  final List<ShoppingItem> items = [];
+
+  @override
+  void dispose() {
+    itemController.dispose();
+    qtyController.dispose();
+    super.dispose();
+  }
+
+
 }
