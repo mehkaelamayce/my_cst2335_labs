62854778e0d9752baa8a880febda79823ed465a0
Mehkaela Dimaano <dima0062@algonquinlive.com>
2026-03-31 22:01:38 -0400

    Added Floor entity for ShoppingItem with auto-generated ID

--- lib/shopping_item.dart
+++ lib/shopping_item.dart
@@ -3,19 +3,13 @@ import 'package:floor/floor.dart';
 @entity
 class ShoppingItem {
 
-  @primaryKey
-  final int id;
+  @PrimaryKey(autoGenerate: true)
+  final int? id;
 
   final String name;
   final int qty;
 
-  static int idCounter = 1;
-
   ///Constructor for creating a shopping item
   ///Updates idCounter so it stays ahead of existing IDs
-  ShoppingItem(this.id, this.name, this.qty) {
-    if (id >= idCounter) {
-      idCounter = id + 1;
-    }
+  ShoppingItem(this.name, this.qty, {this.id}) ;
   }
-}
